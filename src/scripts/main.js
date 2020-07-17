fd70a7472ca04d844c25f7ea16e33e0f3506798b
ardisrtilson <email@example.com>
2020-07-17 15:26:35 -0500

    Added tractor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,3 @@
-import {addPlant, usePlants} from "./field.js"
-import {createAsparagus} from "./seeds/asparagus.js"
-import {createPlan} from "./plan.js"
-
-const plan = createPlan()
-const asparagus = createAsparagus()
-addPlant(asparagus)
-const field = usePlants ()
-const planArray = []
-
-for (const step of plan) { 
-    planArray.push(step)
-    for (const subStep of step){
-    planArray.push(subStep)
-}
-}
-
+import {field} from "./tractor.js"
 console.log("Welcome to the main module")
 console.log(field)
-console.log(plan)
-console.log(planArray)
--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,16 +1,44 @@
+import {addPlant, usePlants} from "./field.js"
+import {createAsparagus} from "./seeds/asparagus.js"
+import {createCorn} from "./seeds/corn.js"
+import {createPotato} from "./seeds/potato.js"
+import {createSoybean} from "./seeds/soybean.js"
+import {createSunflower} from "./seeds/sunflower.js"
+import {createWheat} from "./seeds/wheat.js"
 import {createPlan} from "./plan.js"
 
+const asparagus = createAsparagus()
+const corn = createCorn()
+const potato = createPotato()
+const soybean = createSoybean()
+const sunflower = createSunflower()
+const wheat = createWheat()
 const plan = createPlan()
 
-export const plantSeeds = (plantingPlan) => {
-    for (step of plantingPlan) {
-        
-    }
+
+for (const step of plan) { 
+    for (const subStep of step){
+        switch (subStep) {
+            case "Asparagus":
+                addPlant(asparagus);
+                break;
+            case "Potato":
+                addPlant(potato);
+                break;
+            case "Sunflower":
+                addPlant(sunflower);
+                break;
+            case "Wheat":
+                addPlant(wheat);
+                break;
+            case "Soybean":
+                addPlant(soybean);
+                break;
+            case "Corn":
+                addPlant(corn);
+                break;
+       }
+}
 }
 
-for (const seeds of plantingPlan) {
-
-    }
-}
-
-console.log(plantingPlan)
+export const field = usePlants ()
