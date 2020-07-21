7bb06794e49ab1d6e83357844b3aebcc9a1261aa
ardisrtilson <email@example.com>
2020-07-21 14:03:36 -0500

    Added array test

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,6 +1,11 @@
 const field = []
 
 export const addPlant = (seed) => {
+    if (Array.isArray(seed)){
+        for (const step of seed){
+            field.push(step)
+        }
+    }
     field.push(seed)
 }
 
--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,9 +1,8 @@
 
 export const harvestPlants = (used) => {
 const uses = []
-const cornChecker = []
 for (const step of used){
-    const output = step.output
+    let output = step.output
     const parsedOutput = parseInt(output, 10)
     for (let i = 0; i < parsedOutput; i++) {
         uses.push(step)
